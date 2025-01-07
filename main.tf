52d8f1f665d01da7bc9c9c910a1636145c04c809
mdamodar2000 <mdamodar2000@gmail.com>
2025-01-07 17:53:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,6 @@ region = "us-east-1"
 
 resource "aws_instance" "three" {
   ami           = "ami-0e2c8caa4b6378d8c"
-  count  = 10
+  count  = 5
   instance_type = "t2.micro"
-  tags = {
-    Name = "dev-server"
-  }
 }
