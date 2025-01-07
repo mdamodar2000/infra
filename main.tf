0994d09a2e0e308d1b681086b6fcafa66f57c6dd
mdamodar2000 <mdamodar2000@gmail.com>
2025-01-07 17:25:11 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+region = "us-east-1"
+}
+
+resource "aws_instance" "three" {
+  ami           = "ami-0e2c8caa4b6378d8c"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "dev-server"
+  }
+}
