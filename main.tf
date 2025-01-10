b1d5af45567fcb490bb3fc4d2c7ea1cabfa88d4f
mdamodar2000 <mdamodar2000@gmail.com>
2025-01-10 18:06:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,17 @@ provider "aws" {
 region = "us-east-1"
 }
 
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "demow2d2fd2e2vv2rev"
+
+    workspaces {
+      name = "infra"
+    }
+  }
+}
+
 resource "aws_instance" "three" {
   ami           = "ami-0e2c8caa4b6378d8c"
   instance_type = "t2.micro"
